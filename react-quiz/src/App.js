577293d46b87b0fec035529f5ffeaac91bda5ef4
codeBindHex <xhunter19192020@gmail.com>
2023-10-22 17:40:54 +0300

    first-commit

---
+++ react-quiz/src/App.js
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className="App">
+<h1 className="text-4xl text-red-500 bg-indigo-900">Hello ,you are now in here</h1>
+    </div>
+  );
+}
+
+export default App;
