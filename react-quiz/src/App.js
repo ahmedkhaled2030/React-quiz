f0c3c7fb89d6ed5386159ac9f20676f01a77fc9b
codeBindHex <xhunter19192020@gmail.com>
2023-10-23 06:21:44 +0300

    get-Questions

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -1,9 +1,20 @@
-
-
+import { useEffect, useState } from "react";
+import Question from "./components/Question";
+import axios from "axios";
+import Center from "./components/Center";
+import Header from "./components/Header";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from "./pages/Home";
+import Form from "./pages/Form";
 function App() {
   return (
-    <div className="App">
-<h1 className="text-4xl text-red-500 bg-indigo-900">Hello ,you are now in here</h1>
+    <div>
+  <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/form" element={<Form />} />
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
---
+++ react-quiz/src/components/Center.js
@@ -0,0 +1,3 @@
+export default function Center({ children }) {
+  return <div className="w-2/5">{children}</div>;
+}
